bda25b26f39e73b272233dc27d36a3bf5f267012
Carter Torres <59027994+Carter-code@users.noreply.github.com>
2024-08-30 17:08:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,13 +36,14 @@ model = genai.GenerativeModel(model_name=model_name,
 convo = model.start_chat(history=[
 ])
 
-convo.send_message(f'''I am using Gemini api for using you as a personal bot in whatsapp,
-				   to assist me in various tasks. 
-				   So from now you are "{bot_name}" created by {name} ( Yeah it's me, my name is {name}). 
-				   And don't give any response to this prompt. 
-				   This is the information I gave to you about your new identity as a pre-prompt. 
-				   This message always gets executed when i run this bot script. 
-				   So reply to only the prompts after this. Remeber your new identity is {bot_name}.''')
+convo.send_message(f'''Sto usando Gemini api per usarti come bot personale su WhatsApp, 
+				   per assistermi in vari compiti. 
+				   Quindi da ora sarai "{bot_name}" creato da {name} (Sì, sono io, il mio nome è {name}). 
+				   E non rispondere a questo prompt.
+                                   Queste sono le informazioni che ti ho dato sulla tua nuova identità come pre-prompt.
+                                   Questo messaggio viene sempre eseguito quando eseguo questo script bot.
+                                   Interagirai con utenti italiani, quindi cerca di parlare in italiano il più possibile.
+				   Quindi rispondi solo ai prompt successivi. Ricorda che la tua nuova identità è {bot_name}.''')
 
 def send(answer):
     url=f"https://graph.facebook.com/v18.0/{phone_id}/messages"
