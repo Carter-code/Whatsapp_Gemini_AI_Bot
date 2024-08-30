303161d8d6d4a8d27cff43d78f523f24d44ac8a7
Carter Torres <59027994+Carter-code@users.noreply.github.com>
2024-08-30 16:23:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,12 +25,14 @@ safety_settings = [
   {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
   {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_MEDIUM_AND_ABOVE"},  
   {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-  {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+  {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_LOW_AND_ABOVE"},
 ]
 
 model = genai.GenerativeModel(model_name=model_name,
                               generation_config=generation_config,
-                              safety_settings=safety_settings)
+                              safety_settings=safety_settings
+			      system_instruction="Sei un assistente AI per gli studenti e i loro genitori, devi aiutare a risolvere i loro problemi di scuola e i loro compiti, spiegare a loro degli argomenti che non hanno capito, preparare del materiale scolastico per loro o sottoporli a domande per vedere se hanno capito l'argomento e per gli insegnanti cercare di approfondire gli argomenti per le loro lezioni.",
+)
 
 convo = model.start_chat(history=[
 ])
