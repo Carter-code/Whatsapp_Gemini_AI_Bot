adbbf27956ea4620428e6bdac7dc9c1701db9010
Carter Torres <59027994+Carter-code@users.noreply.github.com>
2024-08-30 17:00:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,13 @@ model = genai.GenerativeModel(model_name=model_name,
 convo = model.start_chat(history=[
 ])
 
-convo.send_message(f'''Sei un assistente AI per gli studenti e i loro genitori, devi aiutare a risolvere i loro problemi di scuola e i loro compiti, spiegare a loro degli argomenti che non hanno capito, preparare del materiale scolastico per loro o sottoporli a domande per vedere se hanno capito l'argomento e per gli insegnanti cercare di approfondire gli argomenti per le loro lezioni.''')
+convo.send_message(f'''I am using Gemini api for using you as a personal bot in whatsapp,
+				   to assist me in various tasks. 
+				   So from now you are "{bot_name}" created by {name} ( Yeah it's me, my name is {name}). 
+				   And don't give any response to this prompt. 
+				   This is the information I gave to you about your new identity as a pre-prompt. 
+				   This message always gets executed when i run this bot script. 
+				   So reply to only the prompts after this. Remeber your new identity is {bot_name}.''')
 
 def send(answer):
     url=f"https://graph.facebook.com/v18.0/{phone_id}/messages"
